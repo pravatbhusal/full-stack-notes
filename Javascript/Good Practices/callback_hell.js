282256e46eb0b0e87f878d1013da17ca33625159
Pravat Bhusal <pravat.bhusal@gmail.com>
2020-07-02 17:09:25 -0500

    Create callback_hell.js

---
+++ Javascript/Good Practices/callback_hell.js
@@ -0,0 +1,36 @@
+// BAD WAY
+const doStuff = (someArray, lookFor) => {
+  const someTracker = [];
+  someArray.forEach(thing => {
+    if (thing) {
+      thing.things.forEach(thingsThing => {
+        if (thingsThing.look === lookFor) {
+          someTracker.push(thingsThing);
+        }
+      })
+    }
+  })
+  return someTracker;
+}
+
+// Better Way
+const otherStuff = (tracker, lookFor) => thingsThing => {
+  if (ThingsThing === lookFor) {
+    tracker.push(thingsThing);
+  }
+}
+const doStuff = (someArray, lookFor) => {
+  const someTracker = [];
+  someArray.forEach(thing => {
+    if (!thing) return;
+    thing.things.forEach(otherStuff(someTracker, lookFor))
+  });
+  return someTracker;
+}
+
+/**
+This is better since now the code inside of the 2nd for-each doesn't have to be re-created each time since it's now referenced in the otherStuff function. This
+in fact also means it increases the performance of our JavaScript code.
+
+Also, the code is much easier to read and handles solving the issue of callback hell.
+*/
