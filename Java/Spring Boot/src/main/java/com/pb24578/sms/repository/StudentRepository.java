7864b837cac6fe64192056e9bae50f314fbc2cea
Pravat <pravat.bhusal@gmail.com>
2022-12-06 21:24:56 -0600

    Add a note about using the SimpleJpaRepository implementation in StudentRepository

--- Java/Spring Boot/src/main/java/com/pb24578/sms/repository/StudentRepository.java
+++ Java/Spring Boot/src/main/java/com/pb24578/sms/repository/StudentRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pb24578.sms.entity.Student;
 
-public interface StudentRepository extends JpaRepository<Student, Long> { // JpaRepository is an API with CRUD, pagination, and sorting
+// JpaRepository is an API with CRUD, pagination, and sorting
+// By default, Spring will create a Bean for StudentRepository using the SimpleJpaRepository implementation
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
